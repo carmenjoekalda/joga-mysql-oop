d5777fa69ed125d81c42ab61ddc8314179295362
carmenjoekalda <cjoekalda@gmail.com>
2024-01-28 16:36:57 +0200

    Create new article using the article oop model

--- routes/article.js
+++ routes/article.js
@@ -8,6 +8,7 @@ const authorController = new AuthorController();
 
 router.get('/', (req, res) => articleController.getAllArticles(req, res));
 router.get('/article/:slug', (req, res) => articleController.getArticleBySlug(req, res));
+router.post('/article/create', (req, res) => articleController.createNewArticle(req, res));
 router.get('/author/:author_id', (req, res) => authorController.getArticlesByAuthor(req, res));
 
 module.exports = router;
